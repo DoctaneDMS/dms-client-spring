9af187f6f72654c9c784cf9d0f4b28b38be12b83
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2021-02-05 18:20:37 -0500

    Test peculiarities related to handling of matrix parameters in jersey

--- src/test/java/com/softwareplumbers/dms/rest/client/spring/TestClient.java
+++ src/test/java/com/softwareplumbers/dms/rest/client/spring/TestClient.java
@@ -5,8 +5,14 @@
  */
 package com.softwareplumbers.dms.rest.client.spring;
 
+import com.softwareplumbers.dms.Document;
+import com.softwareplumbers.dms.DocumentLink;
+import com.softwareplumbers.dms.Exceptions;
+import com.softwareplumbers.dms.Options;
 import com.softwareplumbers.dms.RepositoryService;
 import com.softwareplumbers.dms.Reference;
+import com.softwareplumbers.dms.RepositoryPath;
+import com.softwareplumbers.dms.Workspace;
 import com.softwareplumbers.dms.common.test.DocumentServiceTest;
 import com.softwareplumbers.dms.common.test.TestUtils;
 import com.softwareplumbers.dms.common.test.TestModel;
@@ -19,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static com.softwareplumbers.dms.common.test.TestUtils.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import org.springframework.beans.factory.annotation.Qualifier;
 /**
@@ -72,4 +79,18 @@ public class TestClient extends DocumentServiceTest {
         Reference ref = service.createDocument("", ()->toStream(TestUtils.randomText()), Json.createObjectBuilder().build());
         assertNotNull(ref);
     }
+    
+    @Test
+    public void testThatPathsAreNotIrredemablyMessedUpByMatrixParameters() throws Exceptions.BaseException {
+        // Yep, a semicolon in a path name can really mess things up, because there is literally no way
+        // to stop Jersey from interpreting that as a matrix parameter. 
+        RepositoryPath name1 = randomQualifiedName();
+        service().createWorkspaceByName(name1, Workspace.State.Open, workspaceMetadataModel().generateValue(), Options.CREATE_MISSING_PARENT);
+        String originalText = randomText();
+        Reference ref1 = service().createDocument("text/plain", ()->toStream(originalText), documentMetadataModel().generateValue());
+        RepositoryPath docName = name1.add("aName;withaSemicolon");
+        service().createDocumentLink(docName, ref1, Options.CREATE_MISSING_PARENT);
+	    DocumentLink doc1 = (DocumentLink)service().getObjectByName(docName);
+	    assertEquals(docName, doc1.getName());
+	}  
 }
